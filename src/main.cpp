3c35433ea80559765829f72239ec07e9002e5212
LengyHELL <ubi19998@gmail.com>
2020-11-14 20:22:06 +0100

    Adding new demo :)

--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,19 @@ int main(int argc, char** argv) {
   Engine engine(640, 480, "Test", false);
 
   engine.load_font("lhll.ttf");
-  engine.load_image("img/no_part.png");
+  engine.load_image("img/star.png");
   engine.load_image("img/basic_style.png");
 
-  std::string text = "Somebody once told me, the world is gonna roll me, i aint the sharpest tool in the shed.";
+  std::string text = "SM AS";
 
   Rect button_body(10, 10, 75, 25);
-  TextButton tb1("img/basic_style.png", button_body.copy(20, 10), "Button", 16, {0, 0, 0, 0});
-  ImageButton ib1("img/basic_style.png", button_body.copy(20, 40), "img/no_part.png");
+
   Frame f1("img/basic_style.png", Rect(10, 10, engine.get_width() - 20, engine.get_height() - 20));
-  Picker p1("img/basic_style.png", button_body.copy(20, 70, 0, 25), 16, {0, 0, 0, 0}, 0.5, 23.1, 0.1, 10);
-  TextBox tx1("img/basic_style.png", button_body.copy(20, 125, 75, 75), text, 16, {0, 0, 0, 0});
-  InputBox in1("img/basic_style.png", button_body.copy(20, 230, 75, 0), 16, {0, 0, 0, 0});
+  TextButton tb1("img/basic_style.png", button_body.copy(20, 10), "Button", 16, {0, 0, 0, 0});
+  ImageButton ib1("img/basic_style.png", button_body.copy(20, 40, 0, 25), "img/star.png");
+  Picker p1("img/basic_style.png", button_body.copy(20, 95, 0, 25), 16, {0, 0, 0, 0}, 0.5, 23.1, 0.1, 10);
+  TextBox tx1("img/basic_style.png", button_body.copy(20, 150, 75, 75), text, 16, {0, 0, 0, 0});
+  InputBox in1("img/basic_style.png", button_body.copy(20, 255, 75, 0), 16, {0, 0, 0, 0});
 
   while(!engine.get_exit()) {
     engine.update_inputs();
